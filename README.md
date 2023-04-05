90730351a537a5ce8ec2dbc9001c961b0df34494
filipemelo2002 <filipemelo032@gmail.com>
2023-04-05 12:24:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bytebank
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-hkkfwq)
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NewTransferComponent } from './new-transfer/new-transfer.component';
+
+@NgModule({
+  declarations: [AppComponent, NewTransferComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}

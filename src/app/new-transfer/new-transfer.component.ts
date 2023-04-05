48df5f0856752bc8e9ffc3faa47df8cfb08179a3
filipemelo2002 <filipemelo032@gmail.com>
2023-04-05 12:29:54 -0300

    add initial form template

--- src/app/new-transfer/new-transfer.component.ts
+++ src/app/new-transfer/new-transfer.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-transfer',
   templateUrl: './new-transfer.component.html',
-  styleUrls: ['./new-transfer.component.css']
+  styleUrls: ['./new-transfer.component.scss'],
 })
 export class NewTransferComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
